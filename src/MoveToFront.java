d19a3eb886266ce5d250f0b61f8af501cb795a3a
Levente Lakatos <Levente_Lakatos@epam.com>
2018-03-14 12:51:40 +0100

    MoveToFront finished

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -1,4 +1,3 @@
-import edu.princeton.cs.algs4.BinaryOut;
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
 
@@ -12,9 +11,8 @@ public class MoveToFront {
     public static void encode(){
         while (!BinaryStdIn.isEmpty()){
             char in = BinaryStdIn.readChar();
-            BinaryStdOut.write(indexMove(in));
+            BinaryStdOut.write(indexMove(in), 8);
         }
-        BinaryStdOut.flush();
         BinaryStdOut.close();
     }
 
@@ -33,16 +31,26 @@ public class MoveToFront {
         alphabet[0] = toFind;
         return index;
     }
+
+    private static int indexMove(int toFind){
+        if (toFind < 0  || toFind > R){
+            throw new IllegalArgumentException("Character not in alphabet!");
+        }
+        int index;
+        index = alphabet[toFind];
+        System.arraycopy(alphabet, 0, alphabet, 1, toFind);
+        alphabet[0] = (char) index;
+        return index;
+    }
     /**
      * apply move-to-front decoding, reading from standard input and writing to standard output
      */
     public static void decode(){
-        BinaryOut out = new BinaryOut();
         while (!BinaryStdIn.isEmpty()){
             int in = BinaryStdIn.readChar();
-            out.write(indexMove((char) in));
+            BinaryStdOut.write(indexMove(in));
         }
-        out.close();
+        BinaryStdOut.close();
     }
 
     /**
