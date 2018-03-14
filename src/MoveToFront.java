9edf145bded958fff7b92a0215c9623478eb8d96
Levente Lakatos <lakatos.leventea@gmail.com>
2018-03-14 08:19:45 +0100

    implemented some functions

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -1,8 +1,6 @@
 import edu.princeton.cs.algs4.BinaryOut;
 import edu.princeton.cs.algs4.BinaryStdIn;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
-import java.util.LinkedList;
+import edu.princeton.cs.algs4.BinaryStdOut;
 
 public class MoveToFront {
 
@@ -12,33 +10,40 @@ public class MoveToFront {
      * apply move-to-front encoding, reading from standard input and writing to standard output
      */
     public static void encode(){
-        BinaryOut out = new BinaryOut();
         while (!BinaryStdIn.isEmpty()){
             char in = BinaryStdIn.readChar();
-
+            BinaryStdOut.write(indexMove(in));
         }
-        out.close();
+        BinaryStdOut.flush();
+        BinaryStdOut.close();
     }
 
     private static int indexMove(char toFind){
-        int index = 0;
+        int index = -1;
         for (int i=0;i<alphabet.length; i++){
             if (alphabet[i] == toFind){
                 index = i;
                 break;
             }
+        }
+        if (index == -1){
             throw new IllegalArgumentException("Character not in alphabet!");
         }
-        for (int i=index; i>0; i--){
-            alphabet[i] = alphabet[i - 1];
-        }
+        System.arraycopy(alphabet, 0, alphabet, 1, index);
         alphabet[0] = toFind;
         return index;
     }
     /**
      * apply move-to-front decoding, reading from standard input and writing to standard output
      */
-    public static void decode(){throw new NotImplementedException();}
+    public static void decode(){
+        BinaryOut out = new BinaryOut();
+        while (!BinaryStdIn.isEmpty()){
+            int in = BinaryStdIn.readChar();
+            out.write(indexMove((char) in));
+        }
+        out.close();
+    }
 
     /**
      * if args[0] is '+', apply move-to-front decoding
@@ -46,10 +51,10 @@ public class MoveToFront {
      * @param args
      */
     public static void main(String[] args){
-        if (args[0].equals("+")){
-            encode();
-        }
+        initAlphabet();
         if (args[0].equals("-")){
+            encode();
+        }else if (args[0].equals("+")){
             decode();
         }
     }
